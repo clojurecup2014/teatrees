7987b72f40938a6f1b3c7a320caad6805dd6c298
Mikhail Aksenov <aksenov364@gmail.com>
2014-09-27 19:27:42 +0600

    MOAR TESTS! MOAR!

--- test/teatrees/game_test.clj
+++ test/teatrees/game_test.clj
@@ -4,6 +4,18 @@
 
 (def empty-field [])
 
+
+;;    0 1 2 3 4
+;; 4| 0 0 0 0 1
+;; 3| 0 0 1 0 0
+;; 2| 0 0 1 0 0
+;; 1| 0 0 0 1 0
+;; 0| 0 0 0 0 0
+(def field1 [{ :x 2 :y 3 }
+             { :x 4 :y 4 }
+             { :x 3 :y 1 }
+             { :x 2 :y 2 }])
+
 ;; 0 0 0 0 0 
 ;; 0 0 0 0 0
 ;; 0 1 1 0 0
@@ -36,14 +48,14 @@
 
 ;;    0 1 2 3 4
 ;; 4| 0 0 0 0 0
-;; 3| 0 0 1 0 0
-;; 2| 0 1 1 1 0
-;; 1| 0 0 0 0 0
+;; 3| 0 0 0 0 0
+;; 2| 0 1 0 0 0
+;; 1| 1 1 1 0 0
 ;; 0| 0 0 0 0 0
-(def arrow1 [{ :x 1 :y 2 }
-             { :x 2 :y 2 }
-             { :x 3 :y 2 }
-             { :x 2 :y 3 }]) ;; Check
+(def arrow1 [{ :x 0 :y 1 }
+             { :x 1 :y 1 }
+             { :x 2 :y 1 }
+             { :x 1 :y 2 }]) ;; Check
 
 ;;    0 1 2 3 4
 ;; 4| 0 0 0 1 0
@@ -54,7 +66,7 @@
 (def arrow2 [{ :x 3 :y 4 }
              { :x 3 :y 3 }
              { :x 3 :y 2 }
-             { :x 4 :y 3 }])
+             { :x 4 :y 3 }]) ;; Check
 
 (deftest can-move-test
   (testing "Empty field. Should move anywhere"
@@ -62,11 +74,21 @@
     (is (can-move? empty-field square1 :up))
     (is (can-move? empty-field square1 :down))
     (is (can-move? empty-field square1 :right)))
-  (testing "Empty field. Should move only to right and up"
+  (testing "Empty field. Should move only right and up"
     (is (not (can-move? empty-field square2 :left)))
     (is (can-move? empty-field square2 :up))
     (is (not (can-move? empty-field square2 :down)))
-    (is (can-move? empty-field square2 :right))))
+    (is (can-move? empty-field square2 :right)))
+  (testing "Field1. Should move only down"
+    (is (not (can-move? field1 arrow1 :left)))
+    (is (not (can-move? field1 arrow1 :up)))
+    (is (can-move? field1 arrow1 :down))
+    (is (not (can-move? field1 arrow1 :right))))
+  (testing "Field1. Cannot move anywhere."
+    (is (not (can-move? field1 arrow2 :left)))
+    (is (not (can-move? field1 arrow2 :up)))
+    (is (not (can-move? field1 arrow2 :down)))
+    (is (not (can-move? field1 arrow2 :right)))))
 
 
 
