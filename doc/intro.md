03554469d6fa5d09911027922703eb48896019a0
nidu <kallyamb@gmail.com>
2014-09-27 14:21:27 +0600

    Project created

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to teatrees
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,38 @@
+(defproject teatrees "0.1.0"
+  :description "3d tetris"
+  :url ""
+  :license {:name "EULA"
+            :url ""}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ring/ring-core "1.3.0"]
+                 [org.clojure/java.jdbc "0.3.4"]
+                 [ring-middleware-format "0.3.2"]
+                 [org.clojure/data.json "0.2.5"]
+                 [oracle.jdbc/oracledriver "11.2.0.4"]
+                 [lib-noir "0.8.4"]
+                 [clj-time "0.7.0"]
+                 [org.clojure/clojurescript "0.0-2268"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [om "0.6.5"]
+                 [secretary "1.2.0"]]
+  :plugins [[lein-ring "0.8.11"]
+            [lein-cljsbuild "1.0.3"]
+            [lein-less "1.7.2"]
+            [lein-pdo "0.1.1"]
+            [lein-resource "0.3.6"]
+            [lein-kibit "0.0.8"]]
+  :ring {:handler teatrees.core/app}
+  
+  :less {:source-paths ["src/assets/less"]
+         :target-path "public/css"}
+  
+  :cljsbuild { 
+    :builds [{:id "dev"
+              :source-paths ["src/teatrees-client"]
+              :compiler {
+                :output-to "public/js/teatrees.js"
+                :output-dir "public/js"
+                :optimizations :none
+                :source-map true}}]}
+  :resource {:resource-paths ["vendor"]
+             :target-path "public"})
