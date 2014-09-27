5d8065a0627486d737b3b4c3220ae27862499522
Mikhail Aksenov <aksenov364@gmail.com>
2014-09-27 15:20:48 +0600

    Something works

--- src/teatrees/core.clj
+++ src/teatrees/core.clj
@@ -1,6 +1,22 @@
-(ns teatrees.core)
+(ns teatrees.core
+  (:require [clojure.tools.logging :as log]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [compojure.handler :as handler]
+            [compojure.response :as response]
+            [ring.middleware.format :as format]
+            [teatrees.middleware.key-caser :refer :all]
+            [teatrees.middleware.status-wrapper :refer :all]
+            [teatrees.game-master :as gm]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defroutes game
+  (GET "/available-games" [] (gm/available))
+  (GET "/high-scores" [] (gm/high-scores))
+  (GET "/field-state/:uuid" [uuid] (gm/field-state uuid))
+  (POST "/join" [uuid name] (gm/join uuid name)))
+
+(def app
+  (-> (handler/site game)
+      wrap-status-code
+      wrap-case-change
+      (format/wrap-restful-format :formats [:json-kw :edn])))
