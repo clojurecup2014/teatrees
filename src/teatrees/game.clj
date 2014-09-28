0f6cb139368fd336e03fb969206031fb1cacf04b
Mikhail Aksenov <aksenov364@gmail.com>
2014-09-29 01:41:59 +0600

    Rotation tested

--- src/teatrees/game.clj
+++ src/teatrees/game.clj
@@ -8,7 +8,7 @@
 (def z-max 21)
 (def zborder 10)
 
-(def rate 1000)
+(def rate 3000)
 (def keep-results (* 60 1000))
 
 (def available-games (ref (clojure.lang.PersistentQueue/EMPTY)))
@@ -106,7 +106,7 @@
     (failure "Game not found" :internal-server-error))) 
 
 (defn rotate-figure
-  [uuid dir axis player]
+  [uuid axis dir player]
   (if (@current-games uuid)
     (async/go (async/>! events [:rotate uuid dir axis player]))
     (failure "Game not found" :internal-server-error)))
@@ -153,8 +153,8 @@
 (defn rotate* [direction axis figure center]
   (let [rest-axes (remove #{axis} [:x :y :z])
         rot-fn (case direction
-                 :cw (fn [[x y]] [y (- x)])
-                 :ccw (fn [[x y]] [(- y) x]))]
+                 1 (fn [[x y]] [y (- x)])
+                 -1 (fn [[x y]] [(- y) x]))]
     (for [cell figure
           :let [norm-cell (merge-with - cell center)
                 rot-vals (map norm-cell rest-axes)
@@ -167,12 +167,12 @@
 (defn can-rotate?
   [dir axis figure field zmin zmax]
   (let [center (second figure)
-        prohibited (into #{} (rotate* figure center axis dir))]
+        prohibited (into #{} (rotate* dir axis figure center))]
     (and 
       (nil? (some prohibited field))
       (every? #(and (> % -1) (< % x-max)) (map :x prohibited))
       (every? #(and (> % -1) (< % y-max)) (map :y prohibited))
-      (every? #(and (> % zmin) (< % z-max)) (map :z prohibited)))))
+      (every? #(and (> % zmin) (< % zmax)) (map :z prohibited)))))
 
 (defn rotate
   [direction axis figure field player]
@@ -188,7 +188,6 @@
 
 (defn check-dir 
   [f figure field]
-  
   (nil? (some (prohibited-fields f figure) field)))
 
 (defn can-move?
@@ -220,11 +219,11 @@
   [uuid dir axis player]
   (when-let [cg (@current-games uuid)]
     (let [pl (dec (Integer/parseInt player))
-          fig ((:players cg) pl)
+          fig (get-in cg [:players pl :fig])
           field (cg :field)
           rotated (rotate dir axis fig field player)]
       (dosync
-        (alter current-games assoc-in [uuid :players pl :figure] rotated)))))
+        (alter current-games assoc-in [uuid :players pl :fig] rotated)))))
 
 (defn- calc-shift-clean
   [cell cmp line-vec zb]
